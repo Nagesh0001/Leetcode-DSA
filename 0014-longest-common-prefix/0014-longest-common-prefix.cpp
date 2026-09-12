7333aaf97e705233d31da9af3fb60ea4af973f41
Nagesh Rathore <nageshrathore334@gmail.com>
2026-09-12 21:10:26 +0530

    Time: 0 ms (100%), Space: 12.1 MB (24.62%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n = strs.size();
+        if(n == 0) return "";
+        sort(strs.begin(), strs.end());
+        string first = strs[0];
+        string last = strs[n-1];
+        string ans = "";
+        for(int i=0; i<min(first.size(), last.size()); i++){
+            if(first[i] == last[i]){
+                ans += first[i];
+            }
+            else{
+                break;
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+
+// //.Sorting Method  T C:- O(n log n)
+// class Solution {
+// public:
+//     string longestCommonPrefix(vector<string>& strs) {
+//         if(strs.size() == 0) return "";
+//         sort(strs.begin(), strs.end());
+//         string first = strs[0];
+//         string last = strs[strs.size()-1];
+//         string ans = "";
+//         for(int i=0; i<min(first.size(), last.size()); i++){
+//             if(first[i] == last[i]){
+//                 ans += first[i];
+//             }
+//             else{
+//                 break;
+//             }
+//         }
+//         return ans;
+//     }
+// };

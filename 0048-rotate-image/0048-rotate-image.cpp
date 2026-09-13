3894840fdf619b1d13988a5cefbd7b3f9504a2b7
Nagesh Rathore <nageshrathore334@gmail.com>
2026-09-13 21:45:57 +0530

    Time: 0 ms (100%), Space: 10.1 MB (70.23%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,26 @@
+//.Approach: Transpose + Reverse   T C: O(n²), S C: O(1)
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+         int n = matrix.size();
+        // Step 1: Transpose the matrix
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        // Step 2: Reverse each row
+        for(int i = 0; i < n; i++){
+            int start = 0, end = n - 1;
+            while(start < end){
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+};

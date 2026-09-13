f55ce3993c59c131de7b32d5e03413bdbd069f26
Nagesh Rathore <nageshrathore334@gmail.com>
2026-09-13 21:41:53 +0530

    Time: 0 ms (100%), Space: 11.6 MB (79.55%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,56 @@
+// //One pass solution
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums){
+//         int lo = 0;
+//         int mid = 0;
+//         int hi = nums.size() - 1;
+// //(1). Mid ke baare me socho
+// //(2). 0 to lo-1->0, hi+1->2
+// //(3). lo to mid-1->1
+//         while(mid<=hi){
+//             if(nums[mid] == 2){
+//                 int temp = nums[mid];
+//                 nums[mid] = nums[hi];
+//                 nums[hi] = temp;
+//                 hi--;
+//             }
+//             else if(nums[mid] == 0){
+//                 int temp = nums[mid];
+//                 nums[mid] = nums[lo];
+//                 nums[lo] = temp;
+//                 lo++;
+//                 mid++;
+//             }
+//             else {
+//                 mid++;
+//             }
+//         }
+//         return;
+//     }
+// };
+
+
+//. Two Pass Solution.
+class Solution {
+public:
+    void sortColors(vector<int>& nums){
+        int n = nums.size();
+        int noz = 0;
+        int noo = 0;
+        int notw = 0;
+
+        for(int i=0; i<n; i++){
+            if(nums[i]==0) noz++;
+            else if(nums[i] == 1) noo++;
+            else notw++;
+        }
+        // Fill
+        for(int i=0; i<n; i++){
+            if(i<noz) nums[i] = 0;
+            else if(i<(noz + noo)) nums[i] = 1;
+            else nums[i] = 2;
+        }
+        return;
+    }
+};

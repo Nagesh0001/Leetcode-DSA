7a3849f5f490105d7e99620b2d45b94f21bff841
Nagesh Rathore <nageshrathore334@gmail.com>
2026-09-14 23:10:29 +0530

    Time: 0 ms (100%), Space: 9.9 MB (25.52%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,27 @@
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if (s.length() != t.length()) return false; 
+//         sort(s.begin(), s.end());
+//         sort(t.begin(), t.end());
+//         return s == t;
+//     }
+// };
+
+
+
+
+//.Alternative Method   
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        vector<int> count(26, 0); 
+        for (char c : s) count[c - 'a']++;
+        for (char c : t) count[c - 'a']--;
+        for (int i : count) {
+            if (i != 0) return false;
+        }
+        return true;
+    }
+};
